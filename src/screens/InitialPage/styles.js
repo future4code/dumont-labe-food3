bc5952d46cb9d885de67f2dd0b74514d60afaacd
rafae <rafaelgf@gmail.com>
2020-12-07 17:47:43 -0300

    Criação de Header

--- src/screens/InitialPage/styles.js
+++ src/screens/InitialPage/styles.js
@@ -4,9 +4,10 @@ export const InitialPageContainer = styled.div `
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: black;
-    width: 100vw;
-    height: 100vh;
+    width: 360px;
+    height: 640px;
+    padding: 288px 117px 287px;
+    background-color: #000000;
     margin: 0 auto;
 `
 export const Title = styled.h1 `
