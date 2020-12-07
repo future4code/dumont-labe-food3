31fb231730164d40558312e83eed613dab922d22
José Rafael <rafaelllmoliveira@gmail.com>
2020-12-07 15:47:30 -0300

    fiz o global state, rotas e autenticacao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Router from "./router/router"
+import GlobalState from "./global/globalState"
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+  <GlobalState>
+  <Router/>
+  </GlobalState>
   );
 }
 
