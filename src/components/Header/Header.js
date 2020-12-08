834519e74cf3848b6334ed584d159951f2010e70
rafae <rafaelgf@gmail.com>
2020-12-08 14:30:33 -0300

    Pequenas mudanças de estilização

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { HeaderContainer, PageTitle } from './styles'
+import { ButtonStyled, HeaderContainer, PageTitle } from './styles'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import { Button } from  '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 
 const Header = () => {
+    const history = useHistory()
 
     return (
         <HeaderContainer>
-            <button>Voltar</button>
-            <PageTitle>Título da Página</PageTitle>
+            <ButtonStyled><ArrowBackIosIcon/></ButtonStyled>
+            {history.location.pathname==='/feed'
+                ?
+                <PageTitle>FutureEats</PageTitle>
+                :
+                <PageTitle>Título da Página</PageTitle>
+            }
         </HeaderContainer>
     )
 }
