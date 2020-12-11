711015b81b9a696ad28e3978d2e887ad99a170fb
Joyce Roberta <joycinha0406@gmail.com>
2020-12-11 11:00:59 -0300

    Corrigido bug no ternário do histórico de pedidos

--- src/screens/profilePage/profilePage.js
+++ src/screens/profilePage/profilePage.js
@@ -1,31 +1,39 @@
-import React, { useContext, } from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { baseUrl } from "../../constants/constants";
 import GlobalStateContext from "../../global/globalStateContext";
 import useProtectedPage from "../../hooks/useProtectedPage";
 import useRequestData from "../../hooks/useRequestData";
+import Header from "../../components/Header/Header";
+import { ButtonStyled } from "../../components/Header/styles";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import Edit from "../../assets/img/edit-profile.svg";
 import {
-  goToAdressPage,
+  goToEditAddress,
   goToEditProfile,
   goToFeedPage,
 } from "../../router/coordinator";
-import Header from '../../components/Header/Header'
-import {ButtonStyled} from '../../components/Header/styles'
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
-import Edit from '../../assets/img/edit-profile.svg'
-import { goToEditAddress, goToEditProfile, goToFeedPage } from '../../router/coordinator'
-import CardOrder from "../../components/CardOrder/cardOrder"
-import { EditProfile, Profile, ProfileAddress, ProfileAddressTitle, ProfileCPF, ProfileEmail, ProfileEmailDiv, ProfileName } from './styles'
 import CardOrder from "../../components/CardOrder/cardOrder";
+import {
+  EditProfile,
+  Profile,
+  ProfileAddress,
+  ProfileAddressTitle,
+  ProfileCPF,
+  ProfileEmail,
+  ProfileEmailDiv,
+  ProfileName,
+} from "./styles";
 
 const ProfilePage = () => {
   useProtectedPage();
 
-
   const history = useHistory();
 
   const profileInfo = useRequestData(`${baseUrl}/profile`, undefined);
-  const ordersHistory = useRequestData(`${baseUrl}/orders/history`)
+  const ordersHistory = useRequestData(`${baseUrl}/orders/history`);
+
+  
 
   return profileInfo ? (
     <Profile>
@@ -48,8 +56,8 @@ const ProfilePage = () => {
         <div>
           <div>
             <h3>Histórico de Pedidos</h3>
-            {ordersHistory &&
-              ordersHistory.orders.map((order, id) => {
+            {ordersHistory && ordersHistory.orders.length > 0 ? (
+              ordersHistory.orders.map((order) => {
                 return (
                   <CardOrder
                     key={order.id}
@@ -58,16 +66,22 @@ const ProfilePage = () => {
                     date={order.createdAt}
                   />
                 );
-              })}
+              })
+            ) : (
+              <div>
+                <p>Você não realizou nenhum pedido</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
     </Profile>
   ) : (
     <div>
-      <p>Você não realizou nenhum pedido</p>
+      <p>Carregando...</p>
     </div>
   );
+  
 };
 
 export default ProfilePage;
